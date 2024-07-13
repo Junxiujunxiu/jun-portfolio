745cb365691261c320f32ebc55ae4dab68f653fa
Junxiu Wu <rst2479@autuni.ac.nz>
2024-07-13 20:22:22 +1200

    Remove the clicking box.
    Removing the clickable box that trigger the inventory and added to the navbar.

--- jun-portfolio/components/Character.tsx
+++ jun-portfolio/components/Character.tsx
@@ -1,34 +1,21 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useGLTF, useAnimations } from '@react-three/drei';
 import { useThree, extend } from '@react-three/fiber';
-import Inventory from './Inventory';
 import * as THREE from 'three';
 
-// Extend the Three.js namespace with BoxGeometry and Mesh
 extend({ BoxGeometry: THREE.BoxGeometry, Mesh: THREE.Mesh });
 
-// Define the type for the GLTF result, including scene and animations
 interface GLTFResult {
   scene: THREE.Group;
   animations: THREE.AnimationClip[];
 }
 
-// Separate the props
-interface CharacterProps {
-  onClickHead: () => void;
-} 
-
-// Define the functional component Character that takes the callback function onClickHead as a prop that returns nothing.-> generic type.
-const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
+const Character: React.FC = () => {
   const group = useRef<THREE.Group>(null);
 
-  // Load the GLTF model(scene) and animations using useGLTF hook
   const { scene, animations } = useGLTF('/models/boy/scene.gltf') as unknown as GLTFResult;
-
-  // Extract animations and actions from the loaded animations
   const { actions } = useAnimations(animations, group);
 
-  // Log the animations array to the console
   useEffect(() => {
     console.log('Loaded animations:', animations);
     animations.forEach((clip) => {
@@ -38,36 +25,27 @@ const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
       });
     });
 
-    // Log available actions as an array of strings
     const actionNames = Object.keys(actions);
     console.log('Available actions:', actionNames);
   }, [animations, actions]);
 
-  // Get the current camera and WebGL renderer instance from the useThree hook
   const { camera, gl } = useThree();
 
-   // Adjust camera settings
-   useEffect(() => {
+  useEffect(() => {
     const perspectiveCamera = camera as THREE.PerspectiveCamera;
-    perspectiveCamera.near = 1; // Set near clipping plane
-    perspectiveCamera.far = 1000; // Set far clipping plane
-    perspectiveCamera.fov = 90; // Set field of view
-    perspectiveCamera.position.set(-7, 0, 13.6); // Adjust the camera position
-    
-    perspectiveCamera.updateProjectionMatrix(); // Update the camera projection matrix with the new values
+    perspectiveCamera.near = 1;
+    perspectiveCamera.far = 1000;
+    perspectiveCamera.fov = 90;
+    perspectiveCamera.position.set(-7, 0, 13.6);
+    perspectiveCamera.updateProjectionMatrix();
   }, [camera]);
 
-  const headBoxRef = useRef<THREE.Mesh>(null);
-
-  const [showInventory, setShowInventory] = useState(false);
-
-  // Effect hook to handle animation and click events
   useEffect(() => {
     const playNextAnimation = (index: number) => {
       const animationDurations: { [key: string]: number } = {
-        "Walk": 8000, // 4 seconds
-        "Run": 4000, // 4 seconds
-        "Jump": 2000, // 4 seconds
+        "Walk": 8000,
+        "Run": 4000,
+        "Jump": 2000,
       };
 
       const filteredActionNames = Object.keys(animationDurations);
@@ -83,7 +61,6 @@ const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
         action.reset().fadeIn(0.5).play();
         console.log(`Playing animation "${actionName}"`);
 
-        // Log the position before the animation starts
         if (group.current) {
           console.log('Position before animation:', group.current.position);
         }
@@ -94,7 +71,6 @@ const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
       setTimeout(() => {
         if (action) {
           action.fadeOut(0.5);
-          // Log the position after the animation ends
           if (group.current) {
             console.log('Position after animation:', group.current.position);
           }
@@ -104,28 +80,7 @@ const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
     };
 
     playNextAnimation(0);
-
-    const handlePointerDown = (event: MouseEvent) => {
-      const mouse = new THREE.Vector2();
-      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-      const raycaster = new THREE.Raycaster();
-      raycaster.setFromCamera(mouse, camera);
-
-      const intersects = raycaster.intersectObject(headBoxRef.current!, true);
-      if (intersects.length > 0) {
-        onClickHead();
-        setShowInventory(!showInventory);
-      }
-    };
-
-    gl.domElement.addEventListener('pointerdown', handlePointerDown);
-
-    return () => {
-      gl.domElement.removeEventListener('pointerdown', handlePointerDown);
-    };
-  }, [actions, camera, gl, onClickHead]);
+  }, [actions, camera, gl]);
 
   if (!scene) {
     console.log('GLTF scene is null or undefined.');
@@ -134,17 +89,11 @@ const Character: React.FC<CharacterProps> = ({ onClickHead }) => {
 
   return (
     <group ref={group} dispose={null} scale={[2, 2, 2]} position={[5.5, -0.2, 1]} rotation={[0.1, 2.3, 0.09]}>
-      <primitive object={scene} />  {/* render three.js object directly into react component tree, making it part of UI */}
-      <mesh ref={headBoxRef} position={[-0.4, 2, 0]}>  {/* Head bounding box */}
-        <boxGeometry args={[1, 1, 1]} />  {/* it defines the shape of the mesh with dimension */}
-        <meshBasicMaterial color="red" opacity={0} transparent={true} />  {/* appearance of the mesh -> set false for visualize and debugging */}
-      </mesh>
-      {showInventory && <Inventory />}
+      <primitive object={scene} />
     </group>
   );
 };
 
-// Preload the GLTF model to ensure it's ready when component mounts
 useGLTF.preload('/models/boy/scene.gltf');
 
-export default Character;  // Export the Character component
+export default Character;
--- jun-portfolio/components/Navbar.tsx
+++ jun-portfolio/components/Navbar.tsx
@@ -1,14 +1,15 @@
-// components/Navbar.tsx
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFrame, extend } from '@react-three/fiber';
 import { Html } from '@react-three/drei';
 import * as THREE from 'three';
 import Link from 'next/link';
+import Inventory from './Inventory';
 
 extend({ BoxGeometry: THREE.BoxGeometry, Mesh: THREE.Mesh });
 
 const Navbar: React.FC = () => {
   const navRef = useRef<THREE.Group>(null);
+  const [showInventory, setShowInventory] = useState(false);
 
   useFrame(() => {
     if (navRef.current) {
@@ -16,40 +17,52 @@ const Navbar: React.FC = () => {
     }
   });
 
+  const handleProjectClick = () => {
+    setShowInventory(!showInventory);
+  };
+
   return (
-    <group ref={navRef} position={[2, 1, 6]}>
-      <Html>
-        <div className="navbar">
-          <button className="nav-toggle text-black-10 py-2 px-6 rounded border-2 border-white transition-all duration-300 ease-in-out hover:bg-white hover:text-grey-600 font-deersnow">
-            Jun's journey
-          </button>
-          <div className="nav-content hidden absolute top-full left-0 bg-transparent p-2 rounded shadow-lg transition-all duration-300 ease-in-out w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-auto flex-col items-start">
-            <ul className="list-none p-0 flex flex-col w-full font-deersnow">
-              <li className="my-2">
-                <Link href="/aboutMe" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
-                  About Me
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link href="/skills" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
-                  Skills
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link href="/myGrades" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
-                  My Grades
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link href="/contact" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
-                  Contact
-                </Link>
-              </li>
-            </ul>
+    <>
+      <group ref={navRef} position={[2, 1, 6]}>
+        <Html>
+          <div className="navbar">
+            <button className="nav-toggle text-black-10 py-2 px-6 rounded border-2 border-white transition-all duration-300 ease-in-out hover:bg-white hover:text-grey-600 font-deersnow">
+              Jun's journey
+            </button>
+            <div className="nav-content hidden absolute top-full left-0 bg-transparent p-2 rounded shadow-lg transition-all duration-300 ease-in-out w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl h-auto flex-col items-start">
+              <ul className="list-none p-0 flex flex-col w-full font-deersnow">
+                <li className="my-2">
+                  <Link href="/aboutMe" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
+                    About Me
+                  </Link>
+                </li>
+                <li className="my-2">
+                  <Link href="/skills" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
+                    Skills
+                  </Link>
+                </li>
+                <li className="my-2">
+                  <Link href="/myGrades" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
+                    My Grades
+                  </Link>
+                </li>
+                <li className="my-2">
+                  <Link href="/contact" className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
+                    Contact
+                  </Link>
+                </li>
+                <li className="my-2">
+                  <button onClick={handleProjectClick} className="text-black-10 text-lg transition-all duration-300 ease-in-out hover:text-gray-400">
+                    Projects
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </Html>
-    </group>
+        </Html>
+      </group>
+      {showInventory && <Inventory />}
+    </>
   );
 };
 
